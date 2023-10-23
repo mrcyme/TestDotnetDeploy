59938be972e3e86ced34c3ba8563e59360230964
mrcyme <simeonmcl@gmail.com>
2023-10-23 16:32:30 +0200

    migrations

---
+++ MinimalBookApi/Migrations/20231023142000_booklenght.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalBookApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class booklenght : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Length",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "Books");
+        }
+    }
+}
--- MinimalBookApi/Program.cs
+++ MinimalBookApi/Program.cs
@@ -86,4 +86,5 @@ public class Book
     public int Id { get; set; }
     public required string Title { get; set; }
     public required string Author { get; set; }
+    public int Length { get; set; }
 }
